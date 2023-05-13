cf82d7249b650aebf4af5ddd116a183384275bb6
danielmartonca <martonca.daniel@gmail.com>
2023-05-13 14:59:25 +0300

    Updated form on login page to support validation and constraints

--- OVI-GUI/src/configuration/Config.ts
+++ OVI-GUI/src/configuration/Config.ts
@@ -1,5 +1,13 @@
 export class Config {
     public static readonly debug: boolean = true;
 
-    public static readonly debugNotifications: boolean = false;
+    public static readonly debugNotifications: boolean = Config.debug && true;
+    public static readonly debugNotificationsService: boolean = false;
+
+    public static readonly usernameRegex
+}
+
+export namespace Regex {
+    export const username = RegExp("^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$");
+    export const password = RegExp("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$");
 }
