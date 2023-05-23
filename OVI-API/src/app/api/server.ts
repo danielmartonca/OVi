51e5db566c56418ed6e903186e5da45166ee7c42
danielmartonca <martonca.daniel@gmail.com>
2023-05-24 01:46:54 +0300

    Finished registration API

--- OVI-API/src/app/api/server.ts
+++ OVI-API/src/app/api/server.ts
@@ -7,31 +7,42 @@ import {HttpMethod} from "./model/enum/HttpMethod";
 import {RequestProcessor} from "./http-processor/RequestProcessor";
 import {InvalidControllerEndpointError} from "./errors/InvalidControllerEndpointError";
 import {MissingBodyException} from "./errors/MissingBodyException";
+import {dataAccessor, SqlAccessor} from "./data-access/SqlAccessor";
 
 const dotenv = require('dotenv');
+dotenv.config();
 
+export class OViServer {
+    public readonly host: string;
+    public readonly port: number;
 
-class OViServer {
-    private readonly host: string;
-    private readonly port: number;
+    public get address(): string {
+        return `http://${this.host}:${this.port}`;
+    }
+
+    private dataAccessor: SqlAccessor;
     private readonly server: Server;
 
     private static readonly authenticationController: AuthenticationController = new AuthenticationController();
 
-    constructor(host: string, port: number) {
+    constructor(host: string, port: number, dataAccessor: SqlAccessor) {
         this.host = host;
         this.port = port;
 
+        this.dataAccessor = dataAccessor;
         this.server = this.createServer();
+
+        this.server.on('listening', () => console.log(`OVI-API started...`));
+        this.server.on('close', async () => await this.dataAccessor.close());
     }
 
     private createServer(): Server {
         return http.createServer((request: IncomingMessage, response: ServerResponse) => OViServer.handle(request, response));
     }
 
-    public startServer() {
+    public async startServer() {
+        await this.dataAccessor.connect();
         this.server.listen(this.port, this.host);
-        console.log(`Server started at http://${this.host}:${this.port}`);
     }
 
     private static handle(request: IncomingMessage, response: ServerResponse) {
@@ -41,16 +52,16 @@ class OViServer {
         let bodyStr = '';
         request.on('data', chunk => bodyStr += chunk);
         request.on('error', err => ResponseProcessor.internalServerError(response, err));
-        request.on('end', () => OViServer.work(request, response, bodyStr));
+        request.on('end', async () => await OViServer.work(request, response, bodyStr));
     }
 
-    private static work(request: IncomingMessage, response: ServerResponse, body: string | null) {
+    private static async work(request: IncomingMessage, response: ServerResponse, body: string | null) {
         try {
             const method = HttpMethod.stringToType(request.method!);
             if (body == '') body = null;
 
             if (request.url?.startsWith('/api/auth'))
-                return OViServer.authenticationController.mapEndpoints(method, request.url!, body, response);
+                return await OViServer.authenticationController.mapEndpoints(method, request.url!, body, response);
 
             ResponseProcessor.badRequest(response, new InvalidControllerEndpointError(method, `${request.url!} does not match any controller`));
         } catch (error) {
@@ -65,7 +76,9 @@ class OViServer {
     }
 }
 
-dotenv.config();
 
-const server = new OViServer(process.env.SERVER_ADDRESS!, parseInt(process.env.PORT!));
-server.startServer();
+const host = process.env.SERVER_ADDRESS!;
+const port = parseInt(process.env.PORT!);
+
+const server = new OViServer(host, port, dataAccessor);
+server.startServer().then(() => `Server started at ${server.address}`);
